4e8fb5c928d99b9516817cea95f8b40fea18c4c3
Cass <huladollx@hotmail.com>
2022-10-17 21:33:50 +1000

    testchanges

--- backend/terraform/modules/network.tf
+++ backend/terraform/modules/network.tf
@@ -4,6 +4,7 @@ resource "aws_vpc" "ecsprac-vpc" {
   cidr_block = var.vpc_cidr_block
 }
 
+# fetch available azs
 data "aws_availability_zones" "available" {
   state = "available"
 }
@@ -16,6 +17,7 @@ data "aws_availability_zones" "available" {
     availability_zone = data.aws_availability_zones.available.names[count.index]
 }*/
 
+#create public and private subnets
 resource "aws_subnet" "ecsprac-public" {
   count                   = length(var.public_subnet_cidr)
   vpc_id                  = aws_vpc.ecsprac-vpc.id
@@ -35,4 +37,30 @@ resource "aws_subnet" "ecsprac-private" {
   tags = {
     Name = "ecsprac-private${count.index + 1}"
   }
+}
+
+# igw
+resource "aws_internet_gateway" "igw" {
+    vpc_id = aws_vpc.ecsprac-vpc.id
+    tags = {
+        Name = "${var.prefix}-igw"
+    }
+}
+
+# public rtb and public subnets association
+resource "aws_route_table" "public-rtb" {
+    vpc_id = aws_vpc.ecsprac-vpc.id
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.igw.id
+    }
+    tags = {
+        Name = "${var.prefix}-public-rtb"
+    }
+}
+
+resource "aws_route_table_association" "associate-public-subent" {
+    count = length(var.public_subnet_cidr)
+    subnet_id = aws_subnet.ecsprac-public[count.index].id 
+    route_table_id = aws_route_table.public-rtn.id
 }
